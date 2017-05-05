796db7e361bce001dbea25ca3bdf7e78853b30ca
rajeshbabuk <rajeshbabu.k@htcindia.com>
2017-05-05 15:09:46 +0530

    Recap-702 : Update next run time once job is scheduled.

--- src/main/java/org/main/recap/controller/ScheduleJobsController.java
+++ src/main/java/org/main/recap/controller/ScheduleJobsController.java
@@ -1,9 +1,11 @@
 package org.main.recap.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.main.recap.RecapConstants;
 import org.main.recap.model.ScheduleJobRequest;
 import org.main.recap.model.ScheduleJobResponse;
 import org.main.recap.quartz.SchedulerService;
+import org.quartz.CronExpression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * Created by rajeshbabuk on 5/4/17.
@@ -20,22 +24,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/scheduleService")
 public class ScheduleJobsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ScheduleJobsController.class);
+
     @Autowired
     private SchedulerService schedulerService;
 
-    @RequestMapping(value="/scheduleJob", method = RequestMethod.POST)
+    @RequestMapping(value = "/scheduleJob", method = RequestMethod.POST)
     public ScheduleJobResponse scheduleJob(@RequestBody ScheduleJobRequest scheduleJobRequest) {
         ScheduleJobResponse scheduleJobResponse = new ScheduleJobResponse();
-        if (RecapConstants.SCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
-            String message = schedulerService.scheduleJob(scheduleJobRequest.getJobName(), scheduleJobRequest.getCronExpression());
-            scheduleJobResponse.setMessage(message);
-        } else if (RecapConstants.RESCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
-            String message = schedulerService.rescheduleJob(scheduleJobRequest.getJobName(), scheduleJobRequest.getCronExpression());
-            scheduleJobResponse.setMessage(message);
-        } else if (RecapConstants.UNSCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
-            String message = schedulerService.unscheduleJob(scheduleJobRequest.getJobName());
-            scheduleJobResponse.setMessage(message);
+        String message = null;
+        try {
+            if (RecapConstants.SCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
+                message = schedulerService.scheduleJob(scheduleJobRequest.getJobName(), scheduleJobRequest.getCronExpression());
+            } else if (RecapConstants.RESCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
+                message = schedulerService.rescheduleJob(scheduleJobRequest.getJobName(), scheduleJobRequest.getCronExpression());
+            } else if (RecapConstants.UNSCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
+                message = schedulerService.unscheduleJob(scheduleJobRequest.getJobName());
+            }
+            if (StringUtils.containsIgnoreCase(message, RecapConstants.SUCCESS) && !RecapConstants.UNSCHEDULE.equals(scheduleJobRequest.getScheduleType())) {
+                CronExpression cronExpression = new CronExpression(scheduleJobRequest.getCronExpression());
+                scheduleJobResponse.setNextRunTime(cronExpression.getNextValidTimeAfter(new Date()));
+            }
+        } catch (Exception exception) {
+            logger.error(RecapConstants.LOG_ERROR, exception);
+            message = exception.getMessage();
         }
+        scheduleJobResponse.setMessage(message);
         return scheduleJobResponse;
     }
 }
