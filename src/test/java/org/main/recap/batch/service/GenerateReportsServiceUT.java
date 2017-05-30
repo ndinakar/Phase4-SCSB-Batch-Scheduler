7f904bdb544dd65e1797cab3dce272999f47bca0
angelindayana <angelin.dayana@gmail.com>
2017-05-30 15:35:48 +0530

    Added test case and removed unused imports.

---
+++ src/test/java/org/main/recap/batch/service/GenerateReportsServiceUT.java
@@ -0,0 +1,54 @@
+package org.main.recap.batch.service;
+
+import org.junit.Test;
+import org.main.recap.BaseTestCase;
+import org.main.recap.RecapConstants;
+import org.main.recap.model.batch.SolrIndexRequest;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by angelind on 29/5/17.
+ */
+public class GenerateReportsServiceUT extends BaseTestCase{
+
+    @Value("${server.protocol}")
+    String serverProtocol;
+
+    @Value("${scsb.solr.client.url}")
+    String solrClientUrl;
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @Mock
+    GenerateReportsService generateReportsService;
+
+    @Test
+    public void generateReport() throws Exception {
+        SolrIndexRequest solrIndexRequest = new SolrIndexRequest();
+        solrIndexRequest.setProcessType(RecapConstants.GENERATE_ACCESSION_REPORT);
+        Date createdDate = new Date();
+        solrIndexRequest.setCreatedDate(createdDate);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(RecapConstants.API_KEY, RecapConstants.RECAP);
+        HttpEntity<SolrIndexRequest> httpEntity = new HttpEntity<>(solrIndexRequest, headers);
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(RecapConstants.SUCCESS, HttpStatus.OK);
+        Mockito.when(generateReportsService.getRestTemplate()).thenReturn(restTemplate);
+        Mockito.when(generateReportsService.getSolrIndexRequest(createdDate, RecapConstants.GENERATE_ACCESSION_REPORT)).thenReturn(solrIndexRequest);
+        Mockito.when(generateReportsService.getRestTemplate().exchange(serverProtocol + solrClientUrl + RecapConstants.GENERATE_REPORT_URL, HttpMethod.POST, httpEntity, String.class)).thenReturn(responseEntity);
+        Mockito.when(generateReportsService.generateReport(serverProtocol, solrClientUrl, createdDate, RecapConstants.GENERATE_ACCESSION_REPORT)).thenCallRealMethod();
+        String status = generateReportsService.generateReport(serverProtocol, solrClientUrl, createdDate, RecapConstants.GENERATE_ACCESSION_REPORT);
+        assertNotNull(status);
+        assertEquals(status, RecapConstants.SUCCESS);
+    }
+
+}
