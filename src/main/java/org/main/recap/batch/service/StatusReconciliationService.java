4f8771bc6687941876abb9d1e97e4e52f0708d58
hemalathaselvaraj <hemaselvaraj7@gmail.com>
2017-06-16 15:18:58 +0530

    Added code to schedule job for status reconciliation.

---
+++ src/main/java/org/main/recap/batch/service/StatusReconciliationService.java
@@ -0,0 +1,42 @@
+package org.main.recap.batch.service;
+
+import org.main.recap.RecapConstants;
+import org.main.recap.model.jpa.JobEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by hemalathas on 1/6/17.
+ */
+@Service
+public class StatusReconciliationService {
+
+    private static final Logger logger = LoggerFactory.getLogger(StatusReconciliationService.class);
+
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public String statusReconcilation(String serverProtocol, String scsbCircUrl) {
+        String resultStatus = null;
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(RecapConstants.API_KEY, RecapConstants.RECAP);
+            HttpEntity<JobEntity> httpEntity = new HttpEntity<>(headers);
+            ResponseEntity<String> responseEntity = getRestTemplate().exchange(serverProtocol + scsbCircUrl +RecapConstants.STATUS_RECOCILATION_URL, HttpMethod.GET, httpEntity, String.class);
+            resultStatus = responseEntity.getBody();
+        } catch (RestClientException e) {
+            logger.error(RecapConstants.LOG_ERROR,e);
+            resultStatus = e.getMessage();
+        }
+        return resultStatus;
+    }
+
+}
