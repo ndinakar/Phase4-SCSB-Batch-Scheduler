40c9c3a80b1fbc2e5d40cbe30c2bacf41f704eb0
Hari-Krishnan19 <krish19nov@gmail.com>
2017-07-03 12:28:05 +0530

    Recap806-Batch Job for SubmitCollection

---
+++ src/main/java/org/main/recap/batch/service/SubmitCollectionService.java
@@ -0,0 +1,52 @@
+package org.main.recap.batch.service;
+
+import org.main.recap.RecapConstants;
+import org.main.recap.model.jpa.JobEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by harikrishnanv on 19/6/17.
+ */
+@Service
+public class SubmitCollectionService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SubmitCollectionService.class);
+
+    /**
+     * Gets rest template.
+     *
+     * @return the rest template
+     */
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * This method makes a rest call to scsb circ microservice to initiate the submit collection process.
+     *
+     * @param scsbCircUrl    the scsb circ url
+     * @return status of the submit collection process.
+     */
+    public String submitCollection(String scsbCircUrl) {
+        String resultStatus = null;
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(RecapConstants.API_KEY, RecapConstants.RECAP);
+            HttpEntity<JobEntity> httpEntity = new HttpEntity<>(headers);
+            ResponseEntity<String> responseEntity = getRestTemplate().exchange(scsbCircUrl +RecapConstants.SUBMIT_COLLECTION_URL, HttpMethod.POST, httpEntity, String.class);
+            resultStatus = responseEntity.getBody();
+        } catch (RestClientException e) {
+            logger.error(RecapConstants.LOG_ERROR,e);
+            resultStatus = e.getMessage();
+        }
+        return resultStatus;
+    }
+}
