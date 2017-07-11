3090ceccbea6375fdd946caabbfb8a0698ef0f4f
rajeshbabuk <rajeshbabu.k@htcindia.com>
2017-07-11 11:54:03 +0530

    Recap-805 : Modified code for success and failure messages in jobs and email format.

--- src/main/java/org/main/recap/batch/job/DeletedRecordsExportPulTasklet.java
+++ src/main/java/org/main/recap/batch/job/DeletedRecordsExportPulTasklet.java
@@ -1,14 +1,16 @@
 package org.main.recap.batch.job;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.main.recap.RecapConstants;
 import org.main.recap.batch.service.DeletedRecordsExportPulService;
 import org.main.recap.batch.service.UpdateJobDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -44,18 +46,36 @@ public class DeletedRecordsExportPulTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         logger.info("Executing DeletedRecordsExportPulTasklet");
-        JobExecution jobExecution = chunkContext.getStepContext().getStepExecution().getJobExecution();
-        long jobInstanceId = jobExecution.getJobInstance().getInstanceId();
-        String jobName = jobExecution.getJobInstance().getJobName();
-        Date createdDate = jobExecution.getCreateTime();
-        String jobNameParam = (String) jobExecution.getExecutionContext().get(RecapConstants.JOB_NAME);
-        logger.info("Job Parameter in Deleted Records Export Pul Tasklet : {}", jobNameParam);
-        if (!jobName.equalsIgnoreCase(jobNameParam)) {
-            updateJobDetailsService.updateJob(solrClientUrl, jobName, createdDate, jobInstanceId);
+        StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
+        JobExecution jobExecution = stepExecution.getJobExecution();
+        ExecutionContext executionContext = jobExecution.getExecutionContext();
+        try {
+            String exportStringDate = jobExecution.getJobParameters().getString(RecapConstants.FROM_DATE);
+            long jobInstanceId = jobExecution.getJobInstance().getInstanceId();
+            String jobName = jobExecution.getJobInstance().getJobName();
+            Date createdDate = jobExecution.getCreateTime();
+            String jobNameParam = (String) jobExecution.getExecutionContext().get(RecapConstants.JOB_NAME);
+            logger.info("Job Parameter in Deleted Records Export Pul Tasklet : {}", jobNameParam);
+            if (!jobName.equalsIgnoreCase(jobNameParam)) {
+                updateJobDetailsService.updateJob(solrClientUrl, jobName, createdDate, jobInstanceId);
+            }
+            String resultStatus = deletedRecordsExportPulService.deletedRecordsExportPul(scsbEtlUrl, RecapConstants.DELETED_RECORDS_EXPORT_PUL, createdDate, exportStringDate);
+            logger.info("Deleted Records Export PUL status : {}", resultStatus);
+            if (StringUtils.containsIgnoreCase(resultStatus, RecapConstants.FAIL)) {
+                executionContext.put(RecapConstants.JOB_STATUS, RecapConstants.FAILURE);
+                executionContext.put(RecapConstants.JOB_STATUS_MESSAGE, resultStatus);
+                stepExecution.setExitStatus(new ExitStatus(RecapConstants.FAILURE, resultStatus));
+            } else {
+                executionContext.put(RecapConstants.JOB_STATUS, RecapConstants.SUCCESS);
+                executionContext.put(RecapConstants.JOB_STATUS_MESSAGE, resultStatus);
+                stepExecution.setExitStatus(new ExitStatus(RecapConstants.SUCCESS, resultStatus));
+            }
+        } catch (Exception ex) {
+            logger.error(RecapConstants.LOG_ERROR, ExceptionUtils.getMessage(ex));
+            executionContext.put(RecapConstants.JOB_STATUS, RecapConstants.FAILURE);
+            executionContext.put(RecapConstants.JOB_STATUS_MESSAGE, ExceptionUtils.getMessage(ex));
+            stepExecution.setExitStatus(new ExitStatus(RecapConstants.FAILURE, ExceptionUtils.getFullStackTrace(ex)));
         }
-
-        String status = deletedRecordsExportPulService.deletedRecordsExportPul(scsbEtlUrl, RecapConstants.DELETED_RECORDS_EXPORT_PUL, createdDate);
-        logger.info("Deleted Records Export PUL status : {}", status);
         return RepeatStatus.FINISHED;
     }
 }
