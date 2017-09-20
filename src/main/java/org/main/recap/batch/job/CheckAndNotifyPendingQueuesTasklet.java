2d15d773ff0951d6884e07084f9dc14cb0dd5e52
angelindayana <angelin.dayana@gmail.com>
2017-09-20 16:49:51 +0530

    MS-75 : Added the job to check pending request and notify it by sending email.

---
+++ src/main/java/org/main/recap/batch/job/CheckAndNotifyPendingQueuesTasklet.java
@@ -0,0 +1,68 @@
+package org.main.recap.batch.job;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.main.recap.RecapConstants;
+import org.main.recap.batch.service.CheckAndNotifyPendingQueuesService;
+import org.main.recap.batch.service.UpdateJobDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+
+/**
+ * Created by angelind on 14/9/17.
+ */
+public class CheckAndNotifyPendingQueuesTasklet implements Tasklet {
+
+    private static final Logger logger = LoggerFactory.getLogger(CheckAndNotifyPendingQueuesTasklet.class);
+
+    @Value("${scsb.solr.client.url}")
+    private String solrClientUrl;
+
+    @Value("${scsb.circ.url}")
+    private String scsbCircUrl;
+
+    @Autowired
+    private UpdateJobDetailsService updateJobDetailsService;
+
+    @Autowired
+    private CheckAndNotifyPendingQueuesService checkAndNotifyPendingQueuesService;
+
+    /**
+     * This method starts the execution for checking pending request in the lasOutgoingQ and notifying through sending email.
+     * @param contribution
+     * @param chunkContext
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        logger.info("Executing Pending Queues Check.");
+        StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
+        JobExecution jobExecution = stepExecution.getJobExecution();
+        ExecutionContext executionContext = jobExecution.getExecutionContext();
+        try {
+            long jobInstanceId = jobExecution.getJobInstance().getInstanceId();
+            String jobName = jobExecution.getJobInstance().getJobName();
+            Date createdDate = jobExecution.getCreateTime();
+            updateJobDetailsService.updateJob(solrClientUrl, jobName, createdDate, jobInstanceId);
+            checkAndNotifyPendingQueuesService.checkPendingMsgesInQueue(scsbCircUrl);
+        } catch (Exception ex) {
+            logger.error(RecapConstants.LOG_ERROR, ExceptionUtils.getMessage(ex));
+            executionContext.put(RecapConstants.JOB_STATUS, RecapConstants.FAILURE);
+            executionContext.put(RecapConstants.JOB_STATUS_MESSAGE, RecapConstants.ACCESSION_STATUS_NAME + " " + ExceptionUtils.getMessage(ex));
+            stepExecution.setExitStatus(new ExitStatus(RecapConstants.FAILURE, ExceptionUtils.getFullStackTrace(ex)));
+        }
+        return RepeatStatus.FINISHED;
+    }
+}
