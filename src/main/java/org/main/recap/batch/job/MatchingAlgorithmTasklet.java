5210ad025357c4f63941f7adedd62cc2a77cfcad
rajeshbabuk <rajeshbabu.k@htcindia.com>
2017-10-09 15:25:34 +0530

    MS-62 : Fix for jobs gateway timeout error. Added camel notification and consumer to receive job response messages.

--- src/main/java/org/main/recap/batch/job/MatchingAlgorithmTasklet.java
+++ src/main/java/org/main/recap/batch/job/MatchingAlgorithmTasklet.java
@@ -1,13 +1,16 @@
 package org.main.recap.batch.job;
 
+import org.apache.camel.*;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.main.recap.RecapConstants;
-import org.main.recap.batch.service.MatchingAlgorithmService;
 import org.main.recap.batch.service.UpdateJobDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ExecutionContext;
@@ -17,6 +20,8 @@ import org.springframework.beans.factory.annotation.Value;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by rajeshbabuk on 3/4/17.
@@ -29,10 +34,14 @@ public class MatchingAlgorithmTasklet implements Tasklet{
     private String solrClientUrl;
 
     @Autowired
-    private MatchingAlgorithmService matchingAlgorithmService;
+    private UpdateJobDetailsService updateJobDetailsService;
 
     @Autowired
-    private UpdateJobDetailsService updateJobDetailsService;
+    private CamelContext camelContext;
+
+    @Autowired
+    private ProducerTemplate producerTemplate;
+
 
     /**
      * This method starts the execution of the matching algorithm job.
@@ -63,8 +72,27 @@ public class MatchingAlgorithmTasklet implements Tasklet{
             if (!jobName.equalsIgnoreCase(jobNameParam)) {
                 updateJobDetailsService.updateJob(solrClientUrl, jobName, jobExecution.getCreateTime(), jobInstanceId);
             }
-            String resultStatus = matchingAlgorithmService.initiateMatchingAlgorithm(solrClientUrl, createdDate);
-            logger.info("Matching algorithm status : {}", resultStatus);
+
+            Map<String, String> requestMap = new HashMap<>();
+            requestMap.put(RecapConstants.JOB_ID, String.valueOf(jobExecution.getId()));
+            requestMap.put(RecapConstants.PROCESS_TYPE, RecapConstants.ONGOING_MATCHING_ALGORITHM_JOB);
+            requestMap.put(RecapConstants.CREATED_DATE, createdDate.toString());
+            producerTemplate.sendBody(RecapConstants.MATCHING_ALGORITHM_JOB_INITIATE_QUEUE, requestMap);
+            Endpoint endpoint = camelContext.getEndpoint(RecapConstants.MATCHING_ALGORITHM_JOB_COMPLETION_OUTGOING_QUEUE);
+            PollingConsumer consumer = endpoint.createPollingConsumer();
+            Exchange exchange = consumer.receive();
+            String resultStatus = (String) exchange.getIn().getBody();
+            if (StringUtils.isNotBlank(resultStatus)) {
+                String[] resultSplitMessage = resultStatus.split("\\|");
+                if (!resultSplitMessage[0].equalsIgnoreCase(RecapConstants.JOB_ID + ":" + jobExecution.getId())) {
+                    producerTemplate.sendBody(RecapConstants.MATCHING_ALGORITHM_JOB_COMPLETION_OUTGOING_QUEUE, resultStatus);
+                    resultStatus = RecapConstants.FAILURE + " - " + RecapConstants.FAILURE_QUEUE_MESSAGE;
+                } else {
+                    resultStatus = resultSplitMessage[1];
+                }
+            }
+            logger.info("Job Id : {} Matching Algorithm Job Result Status : {}", jobExecution.getId(), resultStatus);
+
             if (!StringUtils.containsIgnoreCase(resultStatus, RecapConstants.SUCCESS)) {
                 executionContext.put(RecapConstants.JOB_STATUS, RecapConstants.FAILURE);
                 executionContext.put(RecapConstants.JOB_STATUS_MESSAGE, RecapConstants.MATCHING_ALGORITHM_STATUS_NAME + " " + resultStatus);
