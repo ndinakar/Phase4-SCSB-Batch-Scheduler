b6e69942ec1bfbb07d8107bda8c17484e3d33108
rajeshbabuk <rajeshbabu.k@htcindia.com>
2020-08-19 10:37:26 +0530

    Fix for scheduled jobs not running for abandoned jobs.

--- src/main/java/org/recap/quartz/QuartzJobLauncher.java
+++ src/main/java/org/recap/quartz/QuartzJobLauncher.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobLocator;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -87,7 +87,9 @@ public class QuartzJobLauncher extends QuartzJobBean {
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         try {
             Job job = jobLocator.getJob(jobName);
-            JobExecution jobExecution = jobLauncher.run(job, new JobParameters());
+            JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+            jobParametersBuilder.addLong("time", System.currentTimeMillis());
+            JobExecution jobExecution = jobLauncher.run(job, jobParametersBuilder.toJobParameters());
             logger.info("{}_{} was completed successfully. Status : {}", job.getName(), jobExecution.getId(), jobExecution.getStatus());
         } catch (Exception exception) {
             logger.error(RecapCommonConstants.LOG_ERROR, exception);
