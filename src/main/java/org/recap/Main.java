875f4de4b3eb093316929312420316a12fc34963
rajeshbabuk <rajeshbabu.k@htcindia.com>
2021-06-11 01:03:37 +0530

    Fix bus refresh of config server properties. Change properties to get from property util. Fix export issue (added new parameter imsDepositoryCodes).

--- src/main/java/org/recap/Main.java
+++ src/main/java/org/recap/Main.java
@@ -1,7 +1,9 @@
 package org.recap;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +12,7 @@ import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class})
 @PropertySource("classpath:application.properties")
 public class Main {
 
