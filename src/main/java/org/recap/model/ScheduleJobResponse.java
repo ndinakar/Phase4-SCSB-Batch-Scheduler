0a41eb3ae5c79c199ba5dc525aa33e724f31c4d1
rajeshbabuk <rajeshbabu.k@htcindia.com>
2021-08-31 09:47:55 +0530

    SCSB-734: LOMBOK usage

--- src/main/java/org/recap/model/ScheduleJobResponse.java
+++ src/main/java/org/recap/model/ScheduleJobResponse.java
@@ -1,48 +1,16 @@
 package org.recap.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
 /**
  * Created by rajeshbabuk on 5/4/17.
  */
+@Getter
+@Setter
 public class ScheduleJobResponse {
-
     private String message;
     private Date nextRunTime;
-
-    /**
-     * Gets message.
-     *
-     * @return the message
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Sets message.
-     *
-     * @param message the message
-     */
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    /**
-     * Gets next run time.
-     *
-     * @return the next run time
-     */
-    public Date getNextRunTime() {
-        return nextRunTime;
-    }
-
-    /**
-     * Sets next run time.
-     *
-     * @param nextRunTime the next run time
-     */
-    public void setNextRunTime(Date nextRunTime) {
-        this.nextRunTime = nextRunTime;
-    }
 }
--- src/main/java/org/recap/model/batch/SolrIndexRequest.java
+++ src/main/java/org/recap/model/batch/SolrIndexRequest.java
@@ -1,47 +1,16 @@
 package org.recap.model.batch;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
 /**
  * Created by rajeshbabuk on 3/4/17.
  */
+@Getter
+@Setter
 public class SolrIndexRequest {
     private String processType;
     private Date createdDate;
-
-    /**
-     * Gets process type.
-     *
-     * @return the process type
-     */
-    public String getProcessType() {
-        return processType;
-    }
-
-    /**
-     * Sets process type.
-     *
-     * @param processType the process type
-     */
-    public void setProcessType(String processType) {
-        this.processType = processType;
-    }
-
-    /**
-     * Gets created date.
-     *
-     * @return the created date
-     */
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    /**
-     * Sets created date.
-     *
-     * @param createdDate the created date
-     */
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
 }
