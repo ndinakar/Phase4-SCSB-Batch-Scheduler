3f3c6fd06847c2311b334217bb911786884b3801
NSSuresh11 <nssuresh.subramanian@gmail.com>
2022-03-30 10:49:46 +0530

    Fix for Test Coverage

--- src/test/java/org/recap/batch/service/StatusReconciliationServiceUT.java
+++ src/test/java/org/recap/batch/service/StatusReconciliationServiceUT.java
@@ -1,10 +1,7 @@
 package org.recap.batch.service;
 
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Matchers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.recap.BaseTestCaseUT;
 import org.recap.ScsbConstants;
 import org.springframework.http.HttpEntity;
@@ -31,10 +28,10 @@ public class StatusReconciliationServiceUT extends BaseTestCaseUT {
     @Test
     public void testStatusReconciliation() {
         Mockito.when(commonService.getRestTemplate()).thenReturn(restTemplate);
-        Mockito.when(restTemplate.exchange( Matchers.anyString(),
-                Matchers.any(HttpMethod.class),
-                Matchers.<HttpEntity<?>> any(),
-                Matchers.<Class<String>> any())).thenReturn(responseEntity);
+        Mockito.when(restTemplate.exchange( ArgumentMatchers.anyString(),
+                ArgumentMatchers.any(HttpMethod.class),
+                ArgumentMatchers.<HttpEntity<?>> any(),
+                ArgumentMatchers.<Class<String>> any())).thenReturn(responseEntity);
         Mockito.when(responseEntity.getBody()).thenReturn(ScsbConstants.SUCCESS);
         String status=statusReconciliationService.statusReconciliation("scsbCoreUrl");
         assertEquals(ScsbConstants.SUCCESS,status);
