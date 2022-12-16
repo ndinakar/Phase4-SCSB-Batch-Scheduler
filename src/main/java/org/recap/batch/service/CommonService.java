cfef7758be36f0e8c3ddf85e1ef0c95f0a89f759
chittoorcharanraj <chittoor.charanraj@htcinc.com>
2022-12-16 14:51:30 +0530

    Changes for gateway request log email notification

--- src/main/java/org/recap/batch/service/CommonService.java
+++ src/main/java/org/recap/batch/service/CommonService.java
@@ -7,7 +7,11 @@ import org.recap.ScsbConstants;
 import org.recap.model.batch.SolrIndexRequest;
 import org.recap.util.JobDataParameterUtil;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
